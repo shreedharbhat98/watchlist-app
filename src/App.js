c828ba78cbd98b529eb210499e4e24adf078b1e5
Shreedhar Bhat <shridharbhat1998@gmail.com>
2021-12-07 22:13:42 +0530

    Completed basic layout

--- src/App.js
+++ src/App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Layout from './components/Layout';
 import Header from './components/Header';
 import AutoComplete from './components/AutoComplete';
-import ListItem from './components/ListItem';
 
-const App = () => (
-  <React.Fragment>
-    <Layout>
-      <Header title="Watch List" />
-      <AutoComplete />
-      {/* <ListItem /> */}
-    </Layout>
-  </React.Fragment>
+const App = () => {
+  const [stockWatchlist, updateStockWatchlist] = useState([]);
+  const [selectedStocks, updateSelectedStocks] = useState([]);
 
-)
+  const AddToWatchlist = (stock) => {
+    updateStockWatchlist([...stockWatchlist, stock]);
+    updateSelectedStocks([...selectedStocks, stock[0]])
+  }
+
+  const RemoveFromWatchlist = (stock) => {
+    const filteredList = stockWatchlist.filter(item => item[0] !== stock[0]) || [];
+    updateStockWatchlist(filteredList)
+    const filteredSelectedStocks = selectedStocks.filter(item => item !== stock[0]) || []
+    updateSelectedStocks(filteredSelectedStocks);
+  }
+
+  return (
+    <React.Fragment>
+      <Layout>
+        <Header title="Watch List" />
+        <AutoComplete
+          add={AddToWatchlist}
+          stockWatchlist={stockWatchlist}
+          selectedStocks={selectedStocks}
+          remove={RemoveFromWatchlist}
+        />
+      </Layout>
+    </React.Fragment>
+  )
+}
+
 
 export default App;
--- src/components/AutoComplete.js
+++ src/components/AutoComplete.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import WatchlistItmes from '../data/data.json';
 import ListItem from './ListItem';
+import { CloseCircleOutlined } from '@ant-design/icons';
 
-const AutoComplete = () => {
+const AutoComplete = ({ add, remove, stockWatchlist, selectedStocks }) => {
   const [input, setInput] = useState('')
   let suggestions = [];
 
@@ -14,22 +15,60 @@ const AutoComplete = () => {
     })
   }
 
-  console.log('Some', suggestions);
   return (
     <React.Fragment>
-      <input
-        className="appearance-none border border-gray-500 leading-9 rounded w-full py-2 px-3 mt-5 mb-1 focus:outline-none"
-        type="text"
-        value={input}
-        onChange={e => setInput(e.target.value)}
-        placeholder="Search stocks..."
-      />
-
-      <ul className="dropdown-menu absolute top-42 bg-white text-gray-700 w-1/3 z-50 border-l-2 border-indigo-600 pr-4 h-screen">
-        {suggestions?.map((suggestedStock, index) => (
-          <ListItem details={suggestedStock} key={index} fromSuggestion={true} />
-        ))}
+      <div className="relative flex w-full flex-wrap items-stretch mt-2 ">
+        <input
+          value={input}
+          onChange={e => setInput(e.target.value)}
+          type="text"
+          placeholder="Search stocks..."
+          className="px-3 py-3  relative bg-white rounded text-sm border border-gray-500 outline-none focus:outline-none w-full pr-10"
+        />
+        {input.length > 0 ? (
+          <span className="z-10 h-full leading-snug font-normal absolute text-center text-blueGray-300  bg-transparent rounded text-base items-center justify-center w-8 right-0 pr-3 py-2">
+            <CloseCircleOutlined
+              className="text-gray-400 cursor-pointer"
+              onClick={() => setInput('')}
+            />
+          </span>
+        ) : null}
+      </div>
+      <ul className={`absolute top-42 bg-white text-gray-700 w-full md:w-4/6 lg:w-1/3 z-50 border-l-2 border-indigo-600 pr-4 ${suggestions.length && 'max-h-5/6 overflow-scroll'}`}>
+        {suggestions.length > 0 ? suggestions?.map((suggestedStock, index) => (
+          <ListItem
+            details={suggestedStock}
+            add={add}
+            remove={remove}
+            setInput={setInput}
+            key={index}
+            fromSuggestion={true}
+            selectedStocks={selectedStocks}
+          />
+        )) : (
+          <React.Fragment>
+            {input.length > 0 ? (
+              <React.Fragment>
+                <div className="opacity-100 h-18 text-center border-l-2 border-red-400 w-full bg-white absolute grid place-items-center z-100 -ml-0.5">
+                  No results found
+                </div>
+              </React.Fragment>
+            ) : null}
+          </React.Fragment>
+        )}
       </ul>
+      {stockWatchlist?.map((stock, index) => (
+        <ListItem
+          details={stock}
+          key={index}
+          add={add}
+          remove={remove}
+          selectedStocks={selectedStocks}
+        />
+      ))}
+      {!stockWatchlist?.length && (
+        <div className="text-center text-gray-500 pt-5">Looks like empty watchlist</div>
+      )}
     </React.Fragment>
 
   )
