7cbed45f45f82586bd355bc73b2b0c328c0a18f5
Shreedhar Bhat <shridharbhat1998@gmail.com>
2021-12-07 12:25:02 +0530

    setup tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'cyan': {
+          DEFAULT: '#00bcd4',
+          '50': '#e0f7fa',
+          '100': '#b2ebf2',
+          '200': '#80deea',
+          '300': '#4dd0e1',
+          '400': '#26c6da',
+          '500': '#00bcd4',
+          '600': '#00acc1',
+          '700': '#0097a7',
+        }
+      },
+      zIndex: {
+        '100': '100',
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
